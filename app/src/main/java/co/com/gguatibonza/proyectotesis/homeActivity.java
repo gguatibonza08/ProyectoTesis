9700c9428b43bd1d382b1993f00a1545068809c5
Gian Carlos Guatibonza Suarez <gguatibonza08@gmail.com>
2018-10-15 07:50:45 -0500

    se agrego el framelayout

--- app/src/main/java/co/com/gguatibonza/proyectotesis/homeActivity.java
+++ app/src/main/java/co/com/gguatibonza/proyectotesis/homeActivity.java
@@ -2,8 +2,10 @@ package co.com.gguatibonza.proyectotesis;
 
 import android.os.Bundle;
 import android.support.design.bottomappbar.BottomAppBar;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
 
 public class homeActivity extends AppCompatActivity {
     private BottomAppBar appBar;
@@ -14,6 +16,12 @@ public class homeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         appBar = findViewById(R.id.ApBarHome);
         setSupportActionBar(appBar);
+        appBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "prueba", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
